92aed4c66af03e1187484ec4e8644af840153d5f
Bruno Mazziero <bruno.mazziero98@gmail.com>
2025-12-24 21:49:12 -0300

    Panel 1 2 3

--- Swing_UAP/src/main/java/swing_Panels.java
+++ Swing_UAP/src/main/java/swing_Panels.java
@@ -1,2 +1,49 @@
-public class swing_Panels {
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class swing_Panels extends JFrame {
+    private JPanel mainPanel;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JTextField textField;
+    private JLabel replicatorLabel;
+
+    public swing_Panels(){
+
+        inicializingForm();
+//        textField.addActionListener(e -> {
+//            replicateText();
+//        });
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                replicateText();
+            }
+        });
+    }
+
+    private void inicializingForm(){
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,400);
+        setLocationRelativeTo(null);
+    }
+
+    private void replicateText(){
+        this.replicatorLabel.setText(this.textField.getText());
+    }
+
+    public static void main(String[] args) {
+
+        FlatDarculaLaf.setup();
+        swing_Panels swing_panels = new swing_Panels();
+        swing_panels.setVisible(true);
+    }
 }
+
